9d5061a60f7ee6abe2ca647ad09a25041775c600
Punsisi@00 <Johnburns3500@gmail.com>
2024-05-28 06:34:05 +0530

    added admin dashboard js

---
+++ components/Renters/admin dashboad.js
@@ -0,0 +1,17 @@
+//AdminDashboard.js
+
+import React from 'react';
+import RentersSection from './Renters/RentersSection';
+
+const AdminDashboard = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <RentersSection />
+    </div>
+  );
+};
+
+export default AdminDashboard;
+
+
